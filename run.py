691bbe26cbc0586577c936f33f81bbe4407a0acc
Alain2020 <favouralain@yahoo.com>
2023-01-31 23:49:38 +0000

    Add meinloop to the clock

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from tkinter import *
+from time import *
+
+def update():
+    time_string = strftime("%I%M%S")
+    time_label.config(text=time_string)
+window = Tk()
+
+time_label = Label(window,font=("Arial",50),fg="#00FF00",bg="black")
+time_label.pack()
+
+update()
+
+window.mainloop()
